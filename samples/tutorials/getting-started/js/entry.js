5395bea1808eab6a0bb5c2616eb30eee9cb79a6c
Johnny Lim <izeye@naver.com>
2016-09-06 11:53:53 +0900

    Add a webpack sample from http://webpack.github.io/docs/tutorials/getting-started/

---
+++ samples/tutorials/getting-started/js/entry.js
@@ -0,0 +1,4 @@
+//document.write("It works.");
+//require("!style!css!../css/style.css");
+require("../css/style.css");
+document.write(require("./content.js"));
---
+++ samples/tutorials/getting-started/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: "./js/entry.js",
+  output: {
+    path: __dirname,
+    filename: "./js/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      }
+    ]
+  }
+};
